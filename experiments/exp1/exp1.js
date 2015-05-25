8a5983068850ebadeade6934ad28edd219c365f7
Erin <erindb@stanford.edu>
2015-05-25 06:40:14 -0700

    add final lightning round

--- experiments/exp1/exp1.js
+++ experiments/exp1/exp1.js
@@ -2,187 +2,199 @@ var clone = function(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
+var lowercase = function(str) {
+	if (str[0] == "T") {
+		return "t" + str.slice(1,str.length);
+	} else  if (str[0] == "H") {
+		return "h" + str.slice(1,str.length);
+	} else  if (str[0] == "S") {
+		return "s" + str.slice(1,str.length);
+	} else {
+		return str;
+	}
+}
+
+var names = {
+	"male" : _.shuffle([
+		"James", "John", "Robert", "Michael", "William", "David",
+		"Richard", "Joseph", "Charles", "Thomas", "Christopher",
+		"Daniel", "Matthew", "Donald", "Anthony", "Paul", "Mark",
+		"George", "Steven", "Kenneth", "Andrew", "Edward", "Joshua",
+		"Brian", "Kevin", "Ronald", "Timothy", "Jason", "Jeffrey",
+		"Gary", "Ryan", "Nicholas", "Eric", "Jacob", "Jonathan", "Larry",
+		"Frank", "Scott", "Justin", "Brandon", "Raymond", "Gregory",
+		"Samuel", "Benjamin", "Patrick", "Jack", "Dennis", "Jerry",
+		"Alexander", "Tyler"
+	]),
+	"female" : _.shuffle([
+		"Mary", "Jennifer", "Elizabeth", "Linda", "Emily", "Susan",
+		"Margaret", "Jessica", "Dorothy", "Sarah", "Karen", "Nancy",
+		"Betty", "Lisa", "Sandra", "Helen", "Ashley", "Donna", "Kimberly",
+		"Carol", "Michelle", "Emily", "Amanda", "Melissa", "Deborah",
+		"Laura", "Stephanie", "Rebecca", "Sharon", "Cynthia", "Kathleen",
+		"Ruth", "Anna", "Shirley", "Amy", "Angela", "Virginia", "Brenda",
+		"Catherine", "Nicole", "Christina", "Janet", "Samantha", "Carolyn",
+		"Rachel", "Heather", "Diane", "Joyce", "Julie", "Emma"
+	])
+}
+
+var items = [
+	{
+		type: "event",
+		similar_to_whose_experiment: "judith",
+		tag: "joke",
+		backstory: "X told a joke to N kids",
+		give_number_question: "How many of the kids do you think laughed?",
+		binned_histogram_prompt: "Please rate how likely it is that the following numbers of kids laughed.",
+		story: "K of the kids laughed",
+		gender: "male",
+		min: 0,
+		max: 14,
+		bins: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+	},
+	{
+		type: "event",
+		similar_to_whose_experiment: "judith",
+		tag: "marbles",
+		backstory: "X threw N marbles into a pool",
+		give_number_question: "How many of the marbles do you think sank?",
+		binned_histogram_prompt: "Please rate how likely it is that the following numbers of marbles sank.",
+		story: "K of the marbles sank",
+		gender: "female",
+		min: 0,
+		max: 14,
+		bins: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+	},
+	{
+		type: "price",
+		similar_to_whose_experiment: "justine",
+		tag: "watch",
+		backstory: "X bought a watch",
+		give_number_question: "How much do you think it cost?",
+		binned_histogram_prompt: "Please rate how likely it is that the watch cost the following amounts of money.",
+		story: "The watch cost K",
+		gender: "male",
+		min: 0,
+		max: 700,
+		bins: [
+			"$0-$50", "$50-$100", "$100-$150",
+			"$150-$200", "$200-$250", "$250-$300",
+			"$300-$350", "$350-$400", "$400-$450",
+			"$450-$500", "$500-$550", "$550-$600",
+			"$600-$650", "$650-$700", "more than $750"
+		]
+	},
+	{
+		type: "price",
+		similar_to_whose_experiment: "justine",
+		tag: "laptop",
+		backstory: "X bought a laptop",
+		give_number_question: "How much do you think it cost?",
+		binned_histogram_prompt: "Please rate how likely it is that the laptop cost the following amounts of money.",
+		story: "The laptop cost K",
+		gender: "female",
+		min: 0,
+		max: 7500,
+		bins: [
+			"$0-$500", "$500-$1000", "$1000-$1500",
+			"$1500-$2000", "$2000-$2500", "$2500-$3000",
+			"$3000-$3500", "$3500-$4000", "$4000-$4500",
+			"$4500-$5000", "$5000-$5500", "$5500-$6000",
+			"$6000-$6500", "$6500-$7000", "more than $7500"
+		]
+	},
+	{
+		type: "duration",
+		similar_to_whose_experiment: "anthea",
+		tag: "commute",
+		backstory: "X commuted to work yesterday",
+		give_number_question: "How many minutes do you think she spent commuting yesterday?",
+		binned_histogram_prompt: "Please rate how likely it is that she commuted for the following numbers of minutes yesterday.",
+		story: "She commuted for K minutes",
+		gender: "female",
+		min: 0,
+		max: 98,
+		bins: [
+			"0-6", "7-13", "14-20",
+			"21-27", "28-34", "35-41",
+			"42-48", "49-55", "56-62",
+			"63-69", "70-76", "77-83",
+			"84-90", "91-97", "98 or more"
+		]
+	},
+	{
+		type: "duration",
+		similar_to_whose_experiment: "anthea",
+		tag: "tv",
+		backstory: "X watched TV last week",
+		give_number_question: "How many hours do you think he spent watching TV last week?",
+		binned_histogram_prompt: "Please rate how likely it is that he watched TV for the following numbers of hours last week.",
+		story: "He watched TV for K hours",
+		gender: "male",
+		min: 0,
+		max: 43,
+		bins: [
+			"0-3", "4-6", "7-9",
+			"10-12", "13-15", "16-18",
+			"19-21", "22-24", "25-27",
+			"28-30", "31-33", "34-36",
+			"37-39", "40-42", "43 or more"
+		]
+	},
+	{
+		type: "duration",
+		similar_to_whose_experiment: "ciyang",
+		tag: "movies",
+		backstory: "X just went to the movies to see a blockbuster",
+		give_number_question: "How many minutes long do you think the movie was?",
+		binned_histogram_prompt: "Please rate how likely it is that the movie was the following numbers of minutes long.",
+		story: "The movie was K minutes long",
+		gender: "female",
+		min: 0,
+		max: 210,
+		bins: [
+			"0-15", "15-30", "30-45",
+			"45-60", "60-75", "75-90",
+			"90-105", "105-120", "120-135",
+			"135-150", "150-165", "165-180",
+			"180-195", "195-210", "210 or more"
+		]
+	},
+	{
+		type: "duration",
+		similar_to_whose_experiment: "ciyang",
+		tag: "coffee",
+		backstory: "X has just fetched himself a cup of coffee from the office vending machine",
+		give_number_question: "What do you think the temperature of his coffee is?",
+		binned_histogram_prompt: "Please rate how likely it is that his coffee was the following temperatures.",
+		story: "His coffee was K",
+		gender: "male",
+		min: 0,
+		max: 200,
+		bins: [
+			"44°F or less",  "44°F - 56°F",  "56°F - 68°F", 
+			"68°F - 80°F",   "80°F - 92°F",  "92°F - 104°F",
+			"104°F - 116°F",  "116°F - 128°F",   "128°F - 140°F",
+			"140°F - 152°F",  "152°F - 164°F",    "164°F - 176°F",
+			"176°F - 188°F",    "188°F - 200°F",   "200°F or more"
+		]
+	}
+];
+
+var measures = [
+	"give_number",
+	"binned histogram", //15 bins
+	"lightning round"
+];
+
 var nbins = 15;
 var repXN = function(str, name, k) {
 	var newstr = str.replace(" N ", " " + (nbins-1) + " ");
 	newstr = newstr.replace("K", k);
-	return newstr.replace("X", name) + ".";
+	return newstr.replace("X", name);
 }
 
 var get_items = function() {
-
-	var names = {
-		"male" : _.shuffle([
-			"James", "John", "Robert", "Michael", "William", "David",
-			"Richard", "Joseph", "Charles", "Thomas", "Christopher",
-			"Daniel", "Matthew", "Donald", "Anthony", "Paul", "Mark",
-			"George", "Steven", "Kenneth", "Andrew", "Edward", "Joshua",
-			"Brian", "Kevin", "Ronald", "Timothy", "Jason", "Jeffrey",
-			"Gary", "Ryan", "Nicholas", "Eric", "Jacob", "Jonathan", "Larry",
-			"Frank", "Scott", "Justin", "Brandon", "Raymond", "Gregory",
-			"Samuel", "Benjamin", "Patrick", "Jack", "Dennis", "Jerry",
-			"Alexander", "Tyler"
-		]),
-		"female" : _.shuffle([
-			"Mary", "Jennifer", "Elizabeth", "Linda", "Emily", "Susan",
-			"Margaret", "Jessica", "Dorothy", "Sarah", "Karen", "Nancy",
-			"Betty", "Lisa", "Sandra", "Helen", "Ashley", "Donna", "Kimberly",
-			"Carol", "Michelle", "Emily", "Amanda", "Melissa", "Deborah",
-			"Laura", "Stephanie", "Rebecca", "Sharon", "Cynthia", "Kathleen",
-			"Ruth", "Anna", "Shirley", "Amy", "Angela", "Virginia", "Brenda",
-			"Catherine", "Nicole", "Christina", "Janet", "Samantha", "Carolyn",
-			"Rachel", "Heather", "Diane", "Joyce", "Julie", "Emma"
-		])
-	}
-
-	var items = [
-		{
-			type: "event",
-			similar_to_whose_experiment: "judith",
-			tag: "joke",
-			backstory: "X told a joke to N kids",
-			give_number_question: "How many of the kids do you think laughed?",
-			binned_histogram_prompt: "Please rate how likely it is that the following numbers of kids laughed.",
-			story: "K of the kids laughed",
-			gender: "male",
-			min: 0,
-			max: 14,
-			bins: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-		},
-		{
-			type: "event",
-			similar_to_whose_experiment: "judith",
-			tag: "marbles",
-			backstory: "X threw N marbles into a pool",
-			give_number_question: "How many of the marbles do you think sank?",
-			binned_histogram_prompt: "Please rate how likely it is that the following numbers of marbles sank.",
-			story: "K of the marbles sank",
-			gender: "female",
-			min: 0,
-			max: 14,
-			bins: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-		},
-		{
-			type: "price",
-			similar_to_whose_experiment: "justine",
-			tag: "watch",
-			backstory: "X bought a watch",
-			give_number_question: "How much do you think it cost?",
-			binned_histogram_prompt: "Please rate how likely it is that the watch cost the following amounts of money.",
-			story: "The watch cost K",
-			gender: "male",
-			min: 0,
-			max: 700,
-			bins: [
-				"$0-$50", "$50-$100", "$100-$150",
-				"$150-$200", "$200-$250", "$250-$300",
-				"$300-$350", "$350-$400", "$400-$450",
-				"$450-$500", "$500-$550", "$550-$600",
-				"$600-$650", "$650-$700", "more than $750"
-			]
-		},
-		{
-			type: "price",
-			similar_to_whose_experiment: "justine",
-			tag: "laptop",
-			backstory: "X bought a laptop",
-			give_number_question: "How much do you think it cost?",
-			binned_histogram_prompt: "Please rate how likely it is that the laptop cost the following amounts of money.",
-			story: "The laptop cost K",
-			gender: "female",
-			min: 0,
-			max: 7500,
-			bins: [
-				"$0-$500", "$500-$1000", "$1000-$1500",
-				"$1500-$2000", "$2000-$2500", "$2500-$3000",
-				"$3000-$3500", "$3500-$4000", "$4000-$4500",
-				"$4500-$5000", "$5000-$5500", "$5500-$6000",
-				"$6000-$6500", "$6500-$7000", "more than $7500"
-			]
-		},
-		{
-			type: "duration",
-			similar_to_whose_experiment: "anthea",
-			tag: "commute",
-			backstory: "X commuted to work yesterday",
-			give_number_question: "How many minutes do you think she spent commuting yesterday?",
-			binned_histogram_prompt: "Please rate how likely it is that she commuted for the following numbers of minutes yesterday.",
-			story: "X commuted for K minutes",
-			gender: "female",
-			min: 0,
-			max: 98,
-			bins: [
-				"0-6", "7-13", "14-20",
-				"21-27", "28-34", "35-41",
-				"42-48", "49-55", "56-62",
-				"63-69", "70-76", "77-83",
-				"84-90", "91-97", "98 or more"
-			]
-		},
-		{
-			type: "duration",
-			similar_to_whose_experiment: "anthea",
-			tag: "tv",
-			backstory: "X watched TV last week",
-			give_number_question: "How many hours do you think he spent watching TV last week?",
-			binned_histogram_prompt: "Please rate how likely it is that he watched TV for the following numbers of hours last week.",
-			story: "X watched TV for K hours",
-			gender: "male",
-			min: 0,
-			max: 43,
-			bins: [
-				"0-3", "4-6", "7-9",
-				"10-12", "13-15", "16-18",
-				"19-21", "22-24", "25-27",
-				"28-30", "31-33", "34-36",
-				"37-39", "40-42", "43 or more"
-			]
-		},
-		{
-			type: "duration",
-			similar_to_whose_experiment: "ciyang",
-			tag: "movies",
-			backstory: "X just went to the movies to see a blockbuster",
-			give_number_question: "How many minutes long do you think the movie was?",
-			binned_histogram_prompt: "Please rate how likely it is that the movie was the following numbers of minutes long.",
-			story: "The movie was K minutes long",
-			gender: "female",
-			min: 0,
-			max: 210,
-			bins: [
-				"0-15", "15-30", "30-45",
-				"45-60", "60-75", "75-90",
-				"90-105", "105-120", "120-135",
-				"135-150", "150-165", "165-180",
-				"180-195", "195-210", "210 or more"
-			]
-		},
-		{
-			type: "duration",
-			similar_to_whose_experiment: "ciyang",
-			tag: "coffee",
-			backstory: "X has just fetched himself a cup of coffee from the office vending machine",
-			give_number_question: "What do you think the temperature of X's coffee is?",
-			binned_histogram_prompt: "Please rate how likely it is that X's coffee was the following temperatures.",
-			story: "X's coffee was K",
-			gender: "male",
-			min: 0,
-			max: 200,
-			bins: [
-				"44°F or less",  "44°F - 56°F",  "56°F - 68°F", 
-				"68°F - 80°F",   "80°F - 92°F",  "92°F - 104°F",
-				"104°F - 116°F",  "116°F - 128°F",   "128°F - 140°F",
-				"140°F - 152°F",  "152°F - 164°F",    "164°F - 176°F",
-				"176°F - 188°F",    "188°F - 200°F",   "200°F or more"
-			]
-		}
-	];
-
-	var measures = [
-		"give_number",
-		"binned histogram", //15 bins
-		"lightning round"
-	];
 
 	// var types = [
 	// 	"temperature",
@@ -228,6 +240,20 @@ var get_items = function() {
 	return _.shuffle(trials);
 }
 
+var get_final_items = function() {
+	var pairs = [];
+	for (var i=0; i<10; i++) {
+		var choice1 = clone(_.sample(items));
+		var choice2 = clone(_.sample(items));
+		choice1.name = names[choice1.gender].shift();
+		choice2.name = names[choice2.gender].shift();
+		choice1.bin = _.sample(choice1.bins);
+		choice2.bin = _.sample(choice2.bins.filter(function(x) {return x != choice1.bin;}));
+		pairs.push([choice1, choice2]);
+	}
+	return pairs;
+}
+
 function make_slides(f) {
   var   slides = {};
 
@@ -245,6 +271,37 @@ function make_slides(f) {
     }
   });
 
+  slides.final_lightning = slide({
+  	name: "final_lightning",
+  	present: get_final_items(),
+	present_handle: function(pair) {
+    	$(".err").hide();
+    	$("#final_button_container").show();
+    	_s.trial_start = Date.now();
+		$("#final_lightning_sentence").html();
+		$("#final_left_choice").html(
+			repXN(pair[0].backstory, pair[0].name) + " and " +
+			lowercase(repXN(pair[0].story, pair[0].name, pair[0].bin))
+		);
+		$("#final_right_choice").html(
+			repXN(pair[1].backstory, pair[1].name) + " and " +
+			lowercase(repXN(pair[1].story, pair[1].name, pair[1].bin))
+		);
+	},
+	choose: function(direction) {
+		_s.this_trial_data = {};
+		_s.this_trial_data["response"] = $("#final_" + direction + "_choice").html();
+		_s.this_trial_data["chosen_direction"] = direction;
+		_s.this_trial_data["unchosen_contrast"] = $("#final_" + (direction == "right" ? "left" : "right") + "_choice").html();
+		_s.this_trial_data["rt"] = Date.now() - _s.trial_start;
+		exp.data_trials.push(clone(_s.this_trial_data));
+    	$("#final_button_container").hide();
+    	setTimeout(function() {
+    		_stream.apply(_s);
+    	}, 500);
+	},
+  });
+
   slides.trial = slide({
     name : "trial",
     present : get_items(),
@@ -254,7 +311,7 @@ function make_slides(f) {
 		var suffix = stim.type == "temperature" ? "°F" : "";
 		$("#min").html(prefix + stim.min + suffix);
 		$("#max").html(prefix + stim.max + suffix);
-		$("#give_number_sentence").html(repXN(stim.backstory, stim.name));
+		$("#give_number_sentence").html(repXN(stim.backstory, stim.name) + ".");
 		$("#give_number_question").html(stim.give_number_question);
 		_s.init_slider(stim.min, stim.max, prefix, suffix);
     	_s.current_response_data = null;
@@ -262,7 +319,7 @@ function make_slides(f) {
 	binned_histogram: function(stim) {
 		_s.init_sliders();
 		$("#binned_histogram_prompt").html(stim.binned_histogram_prompt);
-		$("#binned_histogram_sentence").html(repXN(stim.backstory, stim.name));
+		$("#binned_histogram_sentence").html(repXN(stim.backstory, stim.name) + ".");
 		for (var i=0; i<nbins; i++) {
 			//$("#" + slider + i)
 			$("#bin" + i).html(stim.bins[i]);
@@ -273,9 +330,9 @@ function make_slides(f) {
     	$("#button_container").show();
     	_s.trial_start = Date.now();
     	var pair = _s.pairs.shift();
-		$("#lightning_sentence").html(repXN(_s.this_trial_data.backstory, _s.this_trial_data.name));
-		$("#left_choice").html(repXN(_s.this_trial_data.story, _s.this_trial_data.name, pair[0]));
-		$("#right_choice").html(repXN(_s.this_trial_data.story, _s.this_trial_data.name, pair[1]));
+		$("#lightning_sentence").html(repXN(_s.this_trial_data.backstory, _s.this_trial_data.name) + ".");
+		$("#left_choice").html(repXN(_s.this_trial_data.story, _s.this_trial_data.name, pair[0]) + ".");
+		$("#right_choice").html(repXN(_s.this_trial_data.story, _s.this_trial_data.name, pair[1]) + ".");
 	},
 	choose: function(direction) {
 		_s.this_trial_data["response"] = $("#" + direction + "_choice").html();
@@ -464,7 +521,9 @@ function init() {
       screenUW: exp.width
     };
   //blocks of the experiment:
-  exp.structure=["i0", "instructions", "trial", 'subj_info', 'thanks'];
+  exp.structure=["i0", "instructions", 
+  "trial", "final_lightning", 
+  'subj_info', 'thanks'];
   
   exp.data_trials = [];
   //make corresponding slides:
