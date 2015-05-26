244ae7efa61f3b6f5afb4876f56e044dba1439bd
Erin <erindb@stanford.edu>
2015-05-26 13:08:42 -0700

    more graphs

--- experiments/exp1/analysis/exp1.R
+++ experiments/exp1/analysis/exp1.R
@@ -20,22 +20,38 @@ binned_histogram = droplevels(subset(d, measure == "binned_histogram"))
 summary(binned_histogram)
 binned_histogram$response = as.numeric(as.character(binned_histogram$response))
 
-binned_histogram_summary = bootsSummary(binned_histogram, measurevar="response",
-                                        groupvars=c("bin", "tag"))
-
+##get bin_num
 all_bins = as.character(unique(binned_histogram$bins))
 all_bins = c(all_bins, all_bins[1])
-lapply(all_bins, function(str) {fromJSON(str)})
+all_bins = lapply(all_bins, function(str) {
+  str = gsub("u", "", str)
+  str = gsub("'", "\"", str)
+  str = gsub(".xb0", "°", str)
+  fromJSON(str)
+})
 names(all_bins) = c("joke", "movies", "tv", "coffee", "watch", "commute", "laptop", "marbles")
+binned_histogram$bin_num = sapply(1:nrow(binned_histogram), function(i) {
+  bin = as.character(binned_histogram$bin)[i]
+  tag = as.character(binned_histogram$tag)[i]
+  which(all_bins[[tag]] == bin)
+})
 
+binned_histogram_summary = bootsSummary(binned_histogram, measurevar="response",
+                                        groupvars=c("bin", "tag", "bin_num"))
 
-binned_histogram_summary$bin_num = 
-
-ggplot(binned_histogram_summary, aes(x=bin, y=response)) +
+ggplot(binned_histogram_summary, aes(x=bin_num, y=response)) +
   geom_bar(stat="identity") +
   facet_wrap(~ tag, scale="free")
 
-agr = aggregate(response ~ bin+tag,data=binned_histogram,FUN="mean")
-agr$CILow = aggregate(response ~ bin+tag,data=binned_histogram,FUN="ci.low")$response
+lightning = subset(d, measure == "lightning")
+lightning$response = paste(lightning$response, ">", lightning$unchosen_contrast)
+lightning$choice = paste(lightning$response, ">", lightning$unchosen_contrast)
 
-str(binned_histogram)
+lightning$response = as.character(lightning$response)
+lightning$unchosen_contrast = as.character(lightning$unchosen_contrast)
+
+lightning$choice = sapply(i:nrow(lightning), function(i) {
+  chosen = lightning$response[i]
+  unchosen = lightning$unchosen_contrast[i]
+  paste(sort(c(chosen, unchosen)), collapse=" ")
+})
